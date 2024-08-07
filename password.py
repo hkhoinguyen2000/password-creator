33c919c1b76eecc932db300e9e7e7581c13552cf
Khoi Nguyen <hkhoinguyen2000@gmail.com>
2024-08-07 00:10:12 -0400

    more symbols, random upper, lower, symbol

--- password.py
+++ password.py
@@ -25,12 +25,59 @@ formattedWord = []
 
 # swap chars with their similar symbosl randomly
 charToSymbol = {
-    "o": 0,
+    "o": '0',
+    'O': '0',
     "s": "$",
-    'a': "@"
-
+    'a': "@",
+    'B': '8',
+    'l': '1',
+    'L': '1',
+    'T': '7',
+    'S': "$5",
+    'A': '4'
 }
+
+# these are random symbols
+randomChars = "-/:;()$&@.,?!"
 
 # iterate through each word
 
+for word in wordList:
+    newWord = ""
+    for char in word:
+        currentChar = ""
+        coinFlip = random.randint(0, 1)
+
+        # flip to lower case
+        if coinFlip == 0:
+            currentChar = char.lower()
+
+            if currentChar in charToSymbol:
+                # second coinflip: pick between symbol and char
+                coinFlip = random.randint(0, 1)
+
+                if coinFlip == 0:
+                    currentChar = random.choice(charToSymbol[currentChar])
+
+        # flip to uppercase
+        else:
+            currentChar = char.upper()
+        
+            if currentChar in charToSymbol:
+                # second coinflip: pick between symbol and a random char equivalent char
+                coinFlip = random.randint(0, 1)
+
+                if coinFlip == 0:
+                    currentChar = random.choice(charToSymbol[currentChar])
+
+
+
+        
+        newWord += currentChar
+    
+    formattedWord.append(newWord)
+
+randomSymbols = "-/:;()&.,?!"
+
+print('-'.join(formattedWord))
 
